f15421c42495f6b2c5bbef11fc1d8495a471dc43
Musiab <musaibjeelani999@gmail.com>
2024-09-24 18:59:46 +0530

    insa

--- backendApp/views.py
+++ backendApp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import Product
+from django.contrib.auth.decorators import login_required
+
+
 # Create your views here.
 
-
+# @login_required
 def index(req):
     return render(req, 'dashboard.html')
 
-
+# @login_required
 def viewproducts(req):
     products = Product.objects.all()
     return render(req, 'viewproducts.html', {'products': products})
 
-
+# @login_required
 def addproducts(req):
     if (req.method == 'POST'):
         name = req.POST.get('productname')
@@ -25,14 +29,29 @@ def addproducts(req):
         return redirect('/reviewGuard/viewproducts')
     return render(req, 'addproducts.html')
 
-
+# @login_required
 def allorders(req):
     return render(req, 'allorders.html')
 
-
+# @login_required
 def users(req):
     return render(req, 'users.html')
 
 
 def login(req):
+    if (req.method == 'POST'):
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+        # authentication
+        user = authenticate(req, username=username, password=password)
+
+        if user is not None:
+            # Log the user in and redirect to a protected page (like admin dashboard)
+            login(req, user)
+            # Replace with your desired redirect URL
+            return redirect('/reviewGuard')
+        else:
+            # Invalid login credentials
+            return redirect('/reviewGuard/login')
+
     return render(req, 'login.html')
