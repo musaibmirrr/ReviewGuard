e1ddfbbd43f00c6fff09e1ca7ffb42bf4436170a
Musiab <musaibjeelani999@gmail.com>
2024-09-27 20:48:32 +0530

    100

--- shop/views.py
+++ shop/views.py
@@ -19,15 +19,11 @@ def get_client_ip(request):
 # Create your views here.
 def index(req):
     
-    if (req.user.is_authenticated):
-        if not req.user.is_superuser:
+    if (req.user.is_authenticated) and (not req.user.is_superuser):
             context = {'name': req.user.username}
             return render(req, 'index.html',context)
-        else:
-            context = {'name': 'User'}
-            return render(req, 'index.html', context)
     else:
-        return render(req, 'index.html')
+        return redirect('/shop/login')
 
 
 def catalog(req):
