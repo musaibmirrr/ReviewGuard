72aac753f5b583d8f272542cdd9a330ec026b3d6
Musiab <musaibjeelani999@gmail.com>
2024-10-19 13:10:02 +0530

    o

--- shop/views.py
+++ shop/views.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from backendApp.models import Product
-from shop.models import Order,Review,Shop_session
+from shop.models import Order, Review, Shop_session
 from textblob import TextBlob
 
 
-#utility function to get ip of client
+# utility function to get ip of client
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -17,20 +17,22 @@ def get_client_ip(request):
     return ip
 
 # Create your views here.
+
+
 def index(req):
     if (req.user.is_authenticated) and (not req.user.is_superuser):
-        shop_user = Shop_session(user_name = req.user.username)
+        shop_user = Shop_session(user_name=req.user.username)
         shop_user.save()
         context = {'name': req.user.username}
-        return render(req, 'index.html',context)
-    
+        return render(req, 'index.html', context)
+
     elif (req.user.is_authenticated) and (req.user.is_superuser):
-       if(not Shop_session.objects.count() > 0): 
+        if (not Shop_session.objects.count() > 0):
             return render(req, 'index.html')
-       else:
+        else:
             shop_user = Shop_session.objects.last()
             context = {'name': shop_user.user_name}
-            return render(req,'index.html',context)
+            return render(req, 'index.html', context)
     else:
         return render(req, 'index.html')
 
@@ -49,73 +51,85 @@ def catalog(req):
 def product_detail(req, id):
     if ((req.user.is_authenticated) and (not req.user.is_superuser)):
         product = get_object_or_404(Product, id=id)
-        isVerified = False       
-        
-        if(req.method == 'POST'):
+        isVerified = False
+
+        if (req.method == 'POST'):
             rating = req.POST.get('rating')
             body = req.POST.get('body')
 
-            #ip code
+            # ip code
             ipAddress = get_client_ip(req)
-            review_count = Review.objects.filter(ipAddress=ipAddress, product=product).count()
+            review_count = Review.objects.filter(
+                ipAddress=ipAddress, product=product).count()
             ipCount = review_count + 1
 
-            #text blob code
+            # text blob code
             blob = TextBlob(body)
             polarity = blob.sentiment.polarity
             subjectivity = blob.sentiment.subjectivity
 
-             #isVerified check
+            # isVerified check
             orders = Order.objects.filter(user=req.user)
             for order in orders:
                 if order.product.id == id:
-                    isVerified = True 
-            
-            
+                    isVerified = True
+
             if polarity > 0.9:
                 if subjectivity <= 0.5:
-                    isFake = False    
+                    isFake = False
                 else:
                     if (isVerified):
                         isFake = False
-                    else:    
+                    else:
                         isFake = True
                         messages.error(req, "Your review seems suspicious.")
             elif polarity < -0.5:
-                 if subjectivity <= 0.5:
+                if subjectivity <= 0.5:
                     isFake = False
-                 else:
-                    if(isVerified):
-                       isFake = False
+                else:
+                    if (isVerified):
+                        isFake = False
                     else:
                         isFake = True
                         messages.error(req, "Your review seems suspicious.")
             else:
                 isFake = False
 
-
             if review_count > 2:
-                messages.error(req, "You have already submitted 3 reviews for this product. Thank you.")
-                review = Review(user=req.user,product=product,rating=rating,body=body,ipAddress=ipAddress,ipCount=ipCount,subjectivity=subjectivity,polarity=polarity,isFake=isFake,isVerified=isVerified)
+                messages.error(
+                    req, "You have already submitted 3 reviews for this product. Thank you.")
+                review = Review(user=req.user, product=product, rating=rating, body=body, ipAddress=ipAddress,
+                                ipCount=ipCount, subjectivity=subjectivity, polarity=polarity, isFake=isFake, isVerified=isVerified)
                 review.save()
                 return redirect(f'/shop/product/{id}')
 
-            
-            review = Review(user=req.user,product=product,rating=rating,body=body,ipAddress=ipAddress,ipCount=ipCount,subjectivity=subjectivity,polarity=polarity,isFake=isFake,isVerified=isVerified)
+            review = Review(user=req.user, product=product, rating=rating, body=body, ipAddress=ipAddress,
+                            ipCount=ipCount, subjectivity=subjectivity, polarity=polarity, isFake=isFake, isVerified=isVerified)
             review.save()
-            messages.success(req,"You successfully made a review!")
+            messages.success(req, "You successfully made a review!")
             return redirect(f'/shop/product/{id}')
-        
-        
+
         all_reviews = Review.objects.filter(product=product)
         reviews = []
         for review in all_reviews:
             if review.isFake is False and review.ipCount <= 3:
-                reviews.append(review)   
-        
+                reviews.append(review)
+
+        allReviews = Review.objects.filter(product=product)
+        sum = 0
+        divisor = allReviews.count()
+        for docs in allReviews:
+            sum = sum + docs.rating
+
+        if (divisor > 0):
+            average = int(sum/divisor)
+        else:
+            average = 0
         context = {'name': req.user.username,
                    'product': product,
-                   'reviews' : reviews
+                   'reviews': reviews,
+                   'average': average,
+                   'divisor': divisor
                    }
         return render(req, 'product_detail.html', context)
     else:
@@ -126,13 +140,13 @@ def orders(req):
     if ((req.user.is_authenticated) and (not req.user.is_superuser)):
         user_orders = Order.objects.filter(user=req.user)
         context = {'name': req.user.username,
-                   'orders' : user_orders}
+                   'orders': user_orders}
         return render(req, 'orders.html', context)
     else:
         return redirect('/shop/login')
 
 
-def checkout(req,id):
+def checkout(req, id):
     if ((req.user.is_authenticated) and (not req.user.is_superuser)):
         product = get_object_or_404(Product, id=id)
 
@@ -143,13 +157,14 @@ def checkout(req,id):
             landmark = req.POST.get('landmark')
             payment = req.POST.get('payment')
 
-            order = Order(user=req.user, product=product, address=address,city=city, state=state, landmark=landmark,payment=payment)
+            order = Order(user=req.user, product=product, address=address,
+                          city=city, state=state, landmark=landmark, payment=payment)
             order.save()
-            messages.success(req,"Your order has been placed successfully!")
+            messages.success(req, "Your order has been placed successfully!")
             return redirect('/shop/orders')
-        
+
         context = {'name': req.user.username,
-                   'product' : product}
+                   'product': product}
         return render(req, 'checkout.html', context)
     else:
         return redirect('/shop/login')
@@ -183,14 +198,14 @@ def user_login(req):
         # Authenticate the user
         user = authenticate(req, username=username, password=password)
         if user is not None:
-           if(not user.is_superuser): 
-            login(req, user)
-            messages.success(req, "Login successful.")
-            # Redirect to dashboard or wherever you like
-            return redirect('/')
-           else:
-            messages.error(req,"You are not a user!")
-            return redirect('/shop/login')
+            if (not user.is_superuser):
+                login(req, user)
+                messages.success(req, "Login successful.")
+                # Redirect to dashboard or wherever you like
+                return redirect('/')
+            else:
+                messages.error(req, "You are not a user!")
+                return redirect('/shop/login')
 
         else:
             messages.error(req, "Invalid username or password!")
@@ -203,8 +218,9 @@ def user_logout(req):
     messages.success(req, "You have been logged out.")
     return redirect('/shop/login')
 
+
 def aboutus(req):
-   context={
-      'name': req.user.username,
-   }
-   return render(req,'aboutus.html',context)
+    context = {
+        'name': req.user.username,
+    }
+    return render(req, 'aboutus.html', context)
